a90172538dcb32cb4073eabdf2186db4346b2f80
Henok Mesfin <henok.mesfin@slalom.com>
2022-04-04 10:25:33 -0400

    first commit

---
+++ src/main/java/com/devopstraining/docker_jenkins_integration_project/App.java
@@ -0,0 +1,13 @@
+package com.devopstraining.docker_jenkins_integration_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
